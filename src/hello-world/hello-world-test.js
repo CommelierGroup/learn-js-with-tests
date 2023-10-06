d36bc0c6deb6308c3cdbe33fa38b053fb1ccc844
Jehwan Yoo <yoojehwan1875@gmail.com>
2023-10-06 15:51:40 +0900

    test: should return 'Hello, {You}!' when hello('You') is called

--- src/hello-world/hello-world-test.js
+++ src/hello-world/hello-world-test.js
@@ -2,9 +2,9 @@ import { it } from 'node:test'
 import { hello } from './hello-world.js'
 import assert from 'node:assert'
 
-it("should return 'Hello, World!' when hello() is called", () => {
-  const got = hello()
-  const want = 'Hello, World!'
+it("should return 'Hello, You!' when hello('You') is called", () => {
+  const got = hello('Je-Hwan')
+  const want = 'Hello, Je-Hwan!'
 
   assert.equal(got, want)
 })
